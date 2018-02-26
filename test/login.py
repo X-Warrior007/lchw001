dc3134b5552344bad8d614ec7706971469d7c490
经理 <manager@163.com>
2018-02-26 19:28:36 +0800

    0001-print triangle by python

--- test/login.py
+++ test/login.py
@@ -1,8 +1,32 @@
-num1 = 100
+#About print the triangle by python
 
-num2 = 200 
+"""
+*
+**
+***
+****
+*****
+"""
+# # 定义一个变量 记录行数
+# i = 1
+# while i <= 5:
+#     print("*")
+#     i += 1
 
+# 定义一个变量 记录列数
+# j = 1
+# while j <= 5:
+#     print("*", end="")
+#     j += 1
 
-sum1 = num1 + num2
-
-sum1 = 300 
+# 定义一个变量记录行数
+i = 1
+while i <= 5:
+    # 定义一个变量 记录列数
+    j = 1
+    while j <= i:
+        print("*", end="")
+        j += 1
+    # 换行
+    print()
+    i += 1
